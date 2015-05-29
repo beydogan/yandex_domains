7fd96360b17fcbf1cdc06559269a402f5f403fdf
Mehmet Beydogan <mbbrain@gmail.com>
2015-05-29 15:15:43 +0300

    Add CRUD for mailboxes and Add YAR docs

---
+++ spec/yandex_domains/mailboxes_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe YandexDomains do
+  describe "email" do
+    it '/add' do
+      pending "TODO"
+    end
+
+    it '/list' do
+      pending "TODO"
+    end
+
+    it '/edit' do
+      pending "TODO"
+    end
+
+    it '/del' do
+      pending "TODO"
+    end
+
+    it '/counters' do
+      pending "TODO"
+    end
+
+  end
+
+end
