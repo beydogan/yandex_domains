b279a38d38807e09c0342622598e96bd35072e3f
Mehmet Beydogan <mbbrain@gmail.com>
2015-06-01 22:19:24 +0300

    Add source to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,36 +5,37 @@ require 'pry'
 
 WebMock.disable_net_connect!
 
+# Source: https://github.com/sferik/twitter/blob/master/spec/helper.rb
 def a_delete(path)
-  a_request(:delete,  YandexDomains::Client.base_uri + path)
+  a_request(:delete, YandexDomains::Client.base_uri + path)
 end
 
 def a_get(path)
-  a_request(:get,  YandexDomains::Client.base_uri + path)
+  a_request(:get, YandexDomains::Client.base_uri + path)
 end
 
 def a_post(path)
-  a_request(:post,  YandexDomains::Client.base_uri + path)
+  a_request(:post, YandexDomains::Client.base_uri + path)
 end
 
 def a_put(path)
-  a_request(:put,  YandexDomains::Client.base_uri + path)
+  a_request(:put, YandexDomains::Client.base_uri + path)
 end
 
 def stub_delete(path)
-  stub_request(:delete,  YandexDomains::Client.base_uri + path)
+  stub_request(:delete, YandexDomains::Client.base_uri + path)
 end
 
 def stub_get(path)
-  stub_request(:get,  YandexDomains::Client.base_uri + path)
+  stub_request(:get, YandexDomains::Client.base_uri + path)
 end
 
 def stub_post(path)
-  stub_request(:post,  YandexDomains::Client.base_uri + path)
+  stub_request(:post, YandexDomains::Client.base_uri + path)
 end
 
 def stub_put(path)
-  stub_request(:put,  YandexDomains::Client.base_uri + path)
+  stub_request(:put, YandexDomains::Client.base_uri + path)
 end
 
 def fixture_path
